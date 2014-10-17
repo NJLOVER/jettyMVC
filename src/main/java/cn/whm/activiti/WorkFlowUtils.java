2f84b926f4781682404a53974fef670398267e0d
wanghm <wanghm@ctyun.cn>
2014-10-17 15:40:48 +0800

    14.10.17commit

--- src/main/java/cn/whm/activiti/WorkFlowUtils.java
+++ src/main/java/cn/whm/activiti/WorkFlowUtils.java
@@ -3,6 +3,8 @@ package cn.whm.activiti;
 import org.activiti.engine.*;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.runtime.ProcessInstanceQuery;
+import org.activiti.engine.task.Attachment;
+import org.activiti.engine.task.Comment;
 import org.activiti.engine.task.Task;
 import org.apache.commons.lang.StringUtils;
 
@@ -33,28 +35,29 @@ public class WorkFlowUtils {
     public void addDeploy(String filePath){
         repositoryService.createDeployment().addClasspathResource(filePath).deploy();
     }
-
+    //启动一个流程实例,并记录发起人ID.
     public String startProcess(String processKey,String userId,Map<String,Object> processDate ){
         if(StringUtils.isNotEmpty(userId)){
-            serAuthor(userId);
+            setAuthor(userId);
         }
         ProcessInstance pinst = runtimeService.startProcessInstanceByKey(processKey,processDate);
+        clearAuthor();
         return pinst.getId();
     }
-
+    //发起一个流程,并记录业务Id.
     public ProcessInstance startProcessInstance(String procKey,String bussinessKey,Map<String,Object> procMap){
         return runtimeService.startProcessInstanceByKey(procKey,bussinessKey,procMap);
     }
-
+    //通过发起流程的业务ID返回业务流程.
     public ProcessInstance getProcInstByBussinessId(String bussinessId){
         return runtimeService.createProcessInstanceQuery().processInstanceBusinessKey(bussinessId).singleResult();
     }
-
-    public void cancelProcess(String processId){
-        ProcessInstance p = getProcessInstanceByID(processId);
-        runtimeService.deleteProcessInstance(p.getProcessInstanceId(),"system cancel");
+    //删除流程实例
+    public void cancelProcessAndHis(String processInstId){
+        runtimeService.deleteProcessInstance(processInstId,"system cancel");
+        historyService.deleteHistoricProcessInstance(processInstId);
     }
-
+    //同过流程实例ID查询流程实例信息
     private ProcessInstance getProcessInstanceByID(String processInstId) {
         ProcessInstance p = runtimeService.createProcessInstanceQuery()
                 .processInstanceId(processInstId).singleResult();
@@ -66,20 +69,48 @@ public class WorkFlowUtils {
         return taskService.createTaskQuery().processInstanceId(procInstId).list();
     }
 
-    //通过流程发起人Id查询所有激活状态的task
+    //通过流程签收人Id查询所签收的task
     public List<Task> getActiveTaskByAssignee(String definitionKey,String userId){
         return taskService.createTaskQuery().processDefinitionKey(definitionKey).taskAssignee(userId)
                 .active().orderByTaskId().desc().orderByTaskCreateTime().desc().list();
     }
-    //通过候选组id查询所拥有任务(单一流程)
+    //设置签收人
+    public void setAssignee(String taskId,String assignee){
+        taskService.claim(taskId,assignee);
+    }
+
+    //通过候选组id查询未签收任务(单一流程)
     public List<Task> getTaskByCandidateGroup(String definitionKey,String groupId){
         return taskService.createTaskQuery().processDefinitionKey(definitionKey)
                 .taskCandidateGroup(groupId).list();
     }
+    //有网关的用户任务完成.
+    public void completeTask(String taskId,Map<String, Object> variables ){
+        taskService.complete(taskId,variables);
+    }
 
-
-
-    public void serAuthor(String userId){
+    //设置流程发起人.
+    public void setAuthor(String userId){
         identityService.setAuthenticatedUserId(userId);
+    }
+    //清空流程发起人
+    public void clearAuthor(){
+        identityService.setAuthenticatedUserId(null);
+    }
+
+    //给任务添加附件信息
+    public void setAttach(String attachName,String taskId,String processInstanceId,String name,String description,String url){
+        Attachment attachment = taskService.createAttachment("comment", taskId, processInstanceId, name, description, url);
+        taskService.saveAttachment(attachment);
+    }
+
+    //添加审核意见,type可以保存是否通过,msg可以保存原因等等
+    public void setConmment(String taskId,String processInstId,String type,String msg){
+        taskService.addComment(taskId,processInstId,type,msg);
+    }
+    //获取整个流程实例的审核意见
+    public List<Comment> getAllComment(String procInstId){
+        List<Comment> comments = taskService.getProcessInstanceComments(procInstId);
+        return comments;
     }
 }
--- src/main/java/cn/whm/portal/account/rest/DologinHandler.java
+++ src/main/java/cn/whm/portal/account/rest/DologinHandler.java
@@ -5,6 +5,7 @@ import cn.whm.rest.handler.AbstractRESTCmdlet;
 import cn.whm.rest.RESTAnnotation;
 import cn.whm.rest.result.JsonRESTResult;
 import cn.whm.utils.SpringContextUtils;
+import org.activiti.engine.task.Task;
 import org.eclipse.jetty.http.HttpMethods;
 import org.eclipse.jetty.server.Request;
 import org.slf4j.Logger;
@@ -12,7 +13,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,12 +25,15 @@ import java.util.Map;
 @Scope(value="prototype")
 public class DologinHandler extends AbstractRESTCmdlet{
     Logger logger = LoggerFactory.getLogger(DologinHandler.class);
+    @Resource
+    private WorkFlowUtils workFlowUtils;
+    private String taskId;
+    private String processInstId="";
     @RESTAnnotation(URL="/dologin", Methods= HttpMethods.GET)
     public JsonRESTResult doLogin(Request request) throws Exception{
 
 //        DynamicActivitiProcessTest dynamicActivitiProcessTest = (DynamicActivitiProcessTest)SpringContextUtils.getBean("bpmnTest");
 //        dynamicActivitiProcessTest.testDynamicDeploy();
-        WorkFlowUtils workFlowUtils = (WorkFlowUtils)SpringContextUtils.getBean("workFlowUtils");
         Map map = new HashMap();
         map.put("test","test");
         String pinstId = workFlowUtils.startProcess("depositflow","test",map);
@@ -40,16 +46,70 @@ public class DologinHandler extends AbstractRESTCmdlet{
         jsonRESTResult.setMsg("login seccuss");
         return  jsonRESTResult;
     }
-    @RESTAnnotation(URL="/apply", Methods= HttpMethods.GET)
-    public JsonRESTResult startTask(Request request) throws Exception{
 
-//        DynamicActivitiProcessTest dynamicActivitiProcessTest = (DynamicActivitiProcessTest)SpringUtilsContext.getBean("bpmnTest");
-//        dynamicActivitiProcessTest.testDynamicDeploy();
+    /*
+    * 初审节点设置了候选人"firstDepositCheckers",
+    */
+    @RESTAnnotation(URL="/getfirst", Methods= HttpMethods.GET)
+    public JsonRESTResult getFirst(Request request) throws Exception{
+        List<Task> tasks = workFlowUtils.getTaskByCandidateGroup("depositflow","secondDepositCheckers");
+        for(Task task : tasks){
+            logger.error(task.toString());
+        }
+        JsonRESTResult jsonRESTResult = new JsonRESTResult();
+        jsonRESTResult.setStatusCode(800);
+        return  jsonRESTResult;
+    }
+    @RESTAnnotation(URL="/firstCheck", Methods= HttpMethods.GET)
+    public JsonRESTResult firstCheck(Request request) throws Exception{
+        String assignee="whm";
+        String comment = "ok";
+        Map<String, Object> valiables = new HashMap<String, Object>();
+        valiables.put("firstCheckAgree",true);
+        taskId = "206";
+        processInstId = "201";
+//        workFlowUtils.setAssignee(taskId,assignee);
+        workFlowUtils.completeTask(taskId,valiables);
+        List<Task> tasks = workFlowUtils.getTaskByCandidateGroup("depositflow","firstDepositCheckers");
+        for(Task task : tasks){
+            logger.error(task.toString());
+        }
+        JsonRESTResult jsonRESTResult = new JsonRESTResult();
+        jsonRESTResult.setStatusCode(800);
+        return  jsonRESTResult;
+    }
+
+    @RESTAnnotation(URL="/allTest", Methods= HttpMethods.GET)
+    public JsonRESTResult allTest(Request request) throws Exception{
+        //1.启动一个提现流程,进入初审环境
+        Map map = new HashMap();
+        map.put("test","test");
+        String proccInstId = workFlowUtils.startProcess("depositflow","wanghm",map);
+        //2.查询初审人员所能见的所有流程.
+        List<Task> tasks = workFlowUtils.getTaskByCandidateGroup("depositflow","firstDepositCheckers");
+        for(Task task : tasks){
+            logger.error(task.toString());
+            taskId = task.getId();
+        }
+        //3.初审通过,包括 审核人员签收初审任务,以及审核完成推动流程.
+        String assignee="whm";
+        String comment = "ok";
+        Map<String, Object> valiables = new HashMap<String, Object>();
+        valiables.put("firstCheckAgree",true);
+        processInstId = proccInstId;
+        workFlowUtils.setAssignee(taskId,assignee);
+        workFlowUtils.setConmment(taskId,processInstId,"true",comment);
+        workFlowUtils.completeTask(taskId,valiables);
+        //4 查询复审人员所能见的所有流程
+        List<Task> task2s = workFlowUtils.getTaskByCandidateGroup("depositflow","secondDepositCheckers");
+        for(Task task : tasks){
+            logger.error(task.toString());
+            taskId = task.getId();
+        }
+        //5.xxx不写了,大概差不多.
 
         JsonRESTResult jsonRESTResult = new JsonRESTResult();
-        jsonRESTResult.setReturnObj("123");
         jsonRESTResult.setStatusCode(800);
-        jsonRESTResult.setMsg("login seccuss");
         return  jsonRESTResult;
     }
 }
